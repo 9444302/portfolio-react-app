3f37189e6efd847049af69ec18bd62f4e91fdf1d
9444302 <90798635+9444302@users.noreply.github.com>
2022-04-08 15:46:29 -0600

    add links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div name='contact' className="w-full h-screen bg-[#0a192f] text-gray-300 flex justify-center items-center">
-            <div className='max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-                <div>
-                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Contact</p>
-                    <p className='my-4'>Submit the form below or shoot me and email - <a className='underline hover:text-pink-600' href="mailto: toripittsb@gmail.com">toripittsb@gmial.com</a></p>
-                </div>
-            <form action="" className='flex flex-col w-full'>
-               <input className='my-4 p-2 bg-[#ccd6f6] text-black' type="text" placeholder='Name' name='name' />
-               <input className='my-4 p-2 bg-[#ccd6f6] text-black' type="email" placeholder='Email' name='email' />
-               <textarea className='my-4 p-2 bg-[#ccd6f6] text-black' name="message" rows="10" placeholder='Message'></textarea>
-               <div>
-               <button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Send</button>
-               </div>
-            </form>
-            </div>
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] text-gray-300 flex justify-center items-center"
+    >
+      <div className="max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Contact
+          </p>
+          <p className="my-4">
+            Submit the form below or shoot me and email -{" "}
+            <a
+              className="underline hover:text-pink-600"
+              href="mailto: toripittsb@gmail.com"
+            >
+              toripittsb@gmial.com
+            </a>
+          </p>
         </div>
-    )
-}
+        <form
+          method="POST"
+          action="https://getform.io/f/1257a118-f1e8-4204-b7e0-03bcad968952"
+          className="flex flex-col w-full"
+        >
+          <input
+            className="my-4 p-2 bg-[#ccd6f6] text-black"
+            type="text"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            className="my-4 p-2 bg-[#ccd6f6] text-black"
+            type="email"
+            placeholder="Email"
+            name="email"
+          />
+          <textarea
+            className="my-4 p-2 bg-[#ccd6f6] text-black"
+            name="message"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <div>
+            <button
+              type="submit"
+              className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,64 +1,82 @@
 import React from 'react'
-import WorkImg from '../assets/workImg.jpeg'
+import Dictionary from '../assets/dictionary-app.png'
+import Weather from '../assets/weather-app.png'
+
 
 const Work = () => {
     return (
-        <div name="work" className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
-            <div className='max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Work</p>
-                <p className='py-4'>Check out my latest work</p>
-            </div>
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div style={{backgroundImage: `url(${WorkImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS aplication
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
+      <div
+        name="work"
+        className="w-full md:h-screen bg-[#0a192f] text-gray-300"
+      >
+        <div className="max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              Work
+            </p>
+            <p className="py-4">Check out my latest work</p>
+          </div>
+          <div className="grid sm:grid-cols-2 gap-4">
+            <div
+              style={{ backgroundImage: `url(${Weather})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS aplication
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    target="_blank"
+                    href="https://priceless-shirley-dcefde.netlify.app"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    target="_blank"
+                    href="https://github.com/9444302/weather-react"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
                 </div>
-                <div style={{backgroundImage: `url(${WorkImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS aplication
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div style={{backgroundImage: `url(${WorkImg})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS aplication
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div
+              style={{ backgroundImage: `url(${Dictionary})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  React JS aplication
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://goofy-varahamihira-08354f.netlify.app"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/9444302/dictionary-project"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Work
