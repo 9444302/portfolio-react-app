517d27094803f15743e2d1933d0ece843fef94e6
9444302 <90798635+9444302@users.noreply.github.com>
2022-04-10 16:11:39 -0600

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,11 +14,10 @@ const Home = () => {
             Tori Pitts
           </h1>
           <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-            I'm a front end developer
+            Front-End Developer
           </h2>
           <p className="text-[#8892b0] py-4 max-w-[700px]">
-            I'm a front end developer that specialized in building web pages.
-            Can build responsive web pages with React
+            I'm a front end developer that specialized in building responsive web pages with React.
           </p>
           <div>
             <Link to="work" smooth={true} duration={500}>
